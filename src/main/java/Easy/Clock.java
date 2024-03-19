90d60b5d813a646547a86a4dcd1c5b9d010abd72
Pavel Davydov <Davydov.works@gmail.com>
2024-03-19 03:05:07 -0400

    add EasyTask

--- src/main/java/Easy/Clock.java
+++ src/main/java/Easy/Clock.java
@@ -1,4 +1,11 @@
 package Easy;
 
+import java.time.LocalDateTime;
+
 public class Clock {
+    public void shoeTime(){
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        System.out.println("Сейчас на часах" + " " + currentTime);
+    }
 }
--- src/main/java/Easy/Main.java
+++ src/main/java/Easy/Main.java
@@ -1,4 +1,9 @@
 package Easy;
 
 public class Main {
+    public static void main(String[] args) {
+        Clock clock = new Clock();
+        System.out.println();
+        clock.shoeTime();
+    }
 }
