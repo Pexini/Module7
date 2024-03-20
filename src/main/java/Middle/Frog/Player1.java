5298005fadcf945325b8ccc59bf3f3de219371e1
Pavel Davydov <Davydov.works@gmail.com>
2024-03-20 04:06:40 -0400

    reformat task3 Frog

---
+++ src/main/java/Middle/Frog/Player1.java
@@ -0,0 +1,15 @@
+package Middle.Frog;
+
+public class Player1 {
+    public void setChoicePlayer1(int choicePlayer1) {
+        this.choicePlayer1 = choicePlayer1;
+    }
+
+    public int getChoicePlayer1() {
+        return choicePlayer1;
+    }
+
+    private int choicePlayer1;
+
+
+}
