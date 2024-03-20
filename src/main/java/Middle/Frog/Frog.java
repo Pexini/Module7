bd76e9decc13745f961176cf4ea8b7812d125f1b
Pavel Davydov <Davydov.works@gmail.com>
2024-03-20 05:47:12 -0400

    reformat task3 Frog

--- src/main/java/Middle/Frog/Frog.java
+++ src/main/java/Middle/Frog/Frog.java
@@ -3,18 +3,28 @@ package Middle.Frog;
 public class Frog {
 
 
-
     private int jumpCount;
-   public void jump(int count){
-       jumpCount += count;
 
-   }
+    public void jump(int count) {
+        jumpCount += count;
+
+    }
+
     public int getJumpCount() {
         return jumpCount;
     }
+
     @Override
-    public String toString(){
-       return "Лягушка сделала " + jumpCount + " прыжков";
+    public String toString() {
+        String ending;
+        if (jumpCount % 10 == 1 && jumpCount != 11) {
+            ending = "прыжок";
+        } else if ((jumpCount % 10 == 2 || jumpCount % 10 == 3 || jumpCount % 10 == 4) && (jumpCount != 12 && jumpCount != 13 && jumpCount != 14)) {
+            ending = "прыжка";
+        } else {
+            ending = "прыжков";
+        }
+        return "Лягушка сделала " + jumpCount + " " + ending;
     }
 
 }
