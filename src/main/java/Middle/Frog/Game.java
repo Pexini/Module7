2724da1a5ae4874c89505f1610776af5315e4cdd
Pavel Davydov <Davydov.works@gmail.com>
2024-03-27 03:10:22 -0400

    add MAX_JUMP in Frog game

--- src/main/java/Middle/Frog/Game.java
+++ src/main/java/Middle/Frog/Game.java
@@ -3,7 +3,10 @@ package Middle.Frog;
 import java.util.Scanner;
 
 public class Game {
+    private static final int WINNER_MAX_JUMP = 30;
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         Frog frog1 = new Frog();
         Frog frog2 = new Frog();
@@ -14,6 +17,7 @@ public class Game {
         System.out.println("Добро пожаловать в игру Лягушки. Каждый из вас должен по очереди бросать кубик и тот чья лягушка первой сделает 30 прыжков - Победил");
         System.out.println("Начинает игру Игрок под номером 1 - кидайте кубик.");
         while (true) {
+            System.out.println();
             System.out.println("Игрок номер 1. Нажмите Enter - что бы бросить кубик");
             scanner.nextLine();
             player1.throwDice();
@@ -31,10 +35,10 @@ public class Game {
             System.out.println("Игрок 1 " + frog1);
             System.out.println("Игрок 2 " + frog2);
 
-            if (frog1.getJumpCount() >= 30) {
+            if (frog1.getJumpCount() >= WINNER_MAX_JUMP) {
                 System.out.println("Поздравляю Игрок 1 победил. " +  frog1);
                 break;
-            } else if (frog2.getJumpCount() >= 30) {
+            } else if (frog2.getJumpCount() >= WINNER_MAX_JUMP) {
                 System.out.println("Поздравляю Игрок 2 победил. " +  frog2);
                 break;
             }
