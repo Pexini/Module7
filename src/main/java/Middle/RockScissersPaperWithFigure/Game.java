063ece6b6ad01ea2a8809b6ff15e19997e4565d6
Pavel Davydov <Davydov.works@gmail.com>
2024-03-27 03:00:09 -0400

    add RockScissorsPaper with Figure

--- src/main/java/Middle/RockScissersPaperWithFigure/Game.java
+++ src/main/java/Middle/RockScissersPaperWithFigure/Game.java
@@ -16,15 +16,15 @@ public class Game {
             System.out.println("Некорректный ввод. Пожалуйста введите число от 0 до 2");
             return;
         }
-        int computerChoice = (int) (Math.random()*2);
+        int computerChoice = (int) (Math.random() * 2);
         Figure userfigure = figures[userChoice];
         Figure computerFigure = figures[computerChoice];
 
-        if (userfigure.isWon(computerFigure)){
+        if (userfigure.isWon(computerFigure)) {
             System.out.println("Поздравляю вы победили" + " Выбор компьютера " + computerChoice);
         } else if (computerFigure.isWon(userfigure)) {
             System.out.println("Извините вы проиграли" + " Выбор компьютера " + computerChoice);
-        }else {
+        } else {
             System.out.println("Ничья " + " Выбор компьютера " + computerChoice);
         }
     }
